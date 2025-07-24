f840608a88ef58ae79d401389fe164f1cccbb31b
Idel <idelfonsosanchez.snchez@gmail.com>
2025-07-24 16:54:25 -0400

    feat: google maps integration

--- presentation/navigator/screens/client/searchMap/ClientSearchMapScreen.tsx
+++ presentation/navigator/screens/client/searchMap/ClientSearchMapScreen.tsx
@@ -1,10 +1,50 @@
-import { Text, View } from "react-native";
+import { Platform, Text, View } from "react-native";
 import styles from "./Styles";
+import { useEffect, useState } from "react";
+import MapView, { Marker, Region } from "react-native-maps";
+import * as Location from 'expo-location'; 
 
 export default function ClientSearchMapScreen(){
+
+    const [location, setLocation] = useState<Region | undefined>(undefined); //se indica que el valor inicial es undefined
+
+    useEffect(() => {  //permite que este codigo se ejecute solo una vez.
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if(status !== 'granted'){
+            console.log('Permisos de ubicacion denegado');
+            return;
+        }
+
+        if(Platform.OS === 'android'){
+            const { status: backGroundStatus } = await Location.requestBackgroundPermissionsAsync();
+            if(backGroundStatus !== 'granted'){
+                console.log('Permiso de ubicacion en segundo plano denegado')
+            }
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922, 
+            longitudeDelta: 0.0421
+        });
+
+      })();
+    }, [])
+    
+    if(!location){
+        return <View style={styles.container}><Text>No se puede obtener la ubicacion</Text></View>;
+    }
     return(
         <View style={styles.container}>
-            <Text>Client Search Map Screen....</Text>
+            <MapView style={ styles.map } initialRegion={location}>
+                <Marker coordinate={{ 
+                    latitude: location!.latitude, // se le coloca ! 
+                    longitude: location!.longitude
+                 }} title="Mi ubicación"/>
+            </MapView>
         </View>
     );
 }
--- presentation/navigator/screens/client/searchMap/Styles.tsx
+++ presentation/navigator/screens/client/searchMap/Styles.tsx
@@ -3,6 +3,10 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    map:{
+        width:'100%',
+        height: '100%'
     }
 });
 
